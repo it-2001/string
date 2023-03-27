2d1c2e2ba7cf9e56746685b4cf07e78651d913a6
it-2001 <it2001@sspu-opava.cz>
2023-03-27 22:35:13 +0200

    concat, trim, and split

--- lib.rs
+++ lib.rs
@@ -7,9 +7,7 @@ use runtime::runtime::runtime_types::Types;
 use runtime::runtime::runtime_types::*;
 use runtime::runtime::*;
 
-pub struct string {
-    
-}
+pub struct string {}
 
 impl runtime::runtime::Library for string {
     fn call(
@@ -17,15 +15,79 @@ impl runtime::runtime::Library for string {
         id: usize,
         mem: PublicData,
     ) -> Result<runtime_types::Types, runtime_error::ErrTypes> {
+        let m = mem.memory;
         match id {
+            // string::concat
             0 => {
-                
+                // take two strings pointers from registers and concat them returning a new string pointer
+                if let Types::Pointer(u_size, PointerTypes::String) = m.registers[POINTER_REG] {
+                    if let Types::Pointer(u_size2, PointerTypes::String) = m.registers[GENERAL_REG1]
+                    {
+                        let mut new_string = m.strings.to_string(u_size);
+                        new_string.push_str(&m.strings.to_string(u_size2));
+                        return Ok(Types::Pointer(
+                            m.strings.from_String(new_string),
+                            PointerTypes::String,
+                        ));
+                    } else {
+                        return Err(runtime_error::ErrTypes::Message(format!(
+                            "Invalid second string pointer",
+                        )));
+                    }
+                } else {
+                    return Err(runtime_error::ErrTypes::Message(format!(
+                        "Invalid first string pointer"
+                    )));
+                }
+            }
+            // string::trim
+            1 => {
+                // take a string pointer from registers and trim it returning a new string pointer
+                if let Types::Pointer(u_size, PointerTypes::String) = m.registers[POINTER_REG] {
+                    let new_string = m.strings.to_string(u_size).trim().to_owned();
+                    return Ok(Types::Pointer(
+                        m.strings.from_String(new_string),
+                        PointerTypes::String,
+                    ));
+                } else {
+                    return Err(runtime_error::ErrTypes::Message(format!(
+                        "Invalid string pointer"
+                    )));
+                }
+            }
+            // string::split
+            2 => {
+                // take a string pointer from registers and split it returning a new string pointer
+                // also take a string pointer from GENERAL_REG1 and split it with it
+                if let Types::Pointer(u_size, PointerTypes::String) = m.registers[POINTER_REG] {
+                    if let Types::Pointer(u_size2, PointerTypes::String) = m.registers[GENERAL_REG1]
+                    {
+                        let new_string = m.strings.to_string(u_size);
+                        let split_string = m.strings.to_string(u_size2);
+                        let split: Vec<&str> = new_string.split(&split_string).collect();
+                        let obj = m.allocate_obj(split.len() + 1);
+                        // set the first element to the length of the array
+                        m.heap.data[obj][0] = Types::NonPrimitive(0);
+                        for idx in m.strings.push_string_array(split) {
+                            m.heap.data[obj][idx + 1] = Types::Pointer(idx, PointerTypes::String);
+                        }
+                        return Ok(Types::Pointer(obj, PointerTypes::Object));
+                    } else {
+                        return Err(runtime_error::ErrTypes::Message(format!(
+                            "Invalid second string pointer",
+                        )));
+                    }
+                }else {
+                    return Err(runtime_error::ErrTypes::Message(format!(
+                        "Invalid first string pointer"
+                    )));
+                }
             }
             _ => {
                 unreachable!("Invalid function id")
             }
         }
-        return Ok(runtime_types::Types::Null);
+        return Ok(runtime_types::Types::Void);
     }
     fn name(&self) -> String {
         return "string".to_owned();
@@ -37,5 +99,5 @@ impl runtime::runtime::Library for string {
 
 #[no_mangle]
 pub fn init(ctx: &mut Context) -> Box<dyn Library> {
-    return Box::new(string {  });
+    return Box::new(string {});
 }
